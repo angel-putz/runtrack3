c5c07d345e1823f90805cf0ae7ceaf54040d7823
angel-putz <angel.putz@laplateforme.io>
2024-02-12 11:24:20 +0100

    job04

---
+++ job04/index.js
@@ -0,0 +1,12 @@
+    function pop_up() {
+        console.log("Hello Javascript !");
+    }
+
+    function bisextile (année){
+        if ((année % 4 ==0 && année % 100!=0) || année % 400 == 0 ) {
+            return true
+        }
+        else {
+            return false
+        }
+    }

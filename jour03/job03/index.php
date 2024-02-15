1a13051cb785589268abe3a4ef522e5741843414
angel-putz <angel.putz@laplateforme.io>
2024-02-15 16:28:10 +0100

    upadte jour03/job03

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -3,18 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div id="game"></div>
+    <p id="bravo"></p>
 
 
 
 
 
-
-<script src="jquery-3.7.1.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<script src="script.js"></script>
+    <script src="jquery-3.7.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <script src="script.js"></script>
 </body>
 </html>
---
+++ jour03/job03/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    var tiles = [
+        { src: 'logo1.png' ,id: 'tile1', correctPosition: 1 },
+        { src: 'logo2.png' ,id: 'tile2', correctPosition: 2 },
+        { src: 'logo3.png' ,id: 'tile3', correctPosition: 3 },
+        { src: 'logo4.png' ,id: 'tile4', correctPosition: 4 },
+        { src: 'logo5.png' ,id: 'tile5', correctPosition: 5 },
+        { src: 'logo6.png' ,id: 'tile6', correctPosition: 6 },
+        { src: 'logo7.png' ,id: 'tile7', correctPosition: 7 },
+        { src: 'logo8.png' ,id: 'tile8', correctPosition: 8 },
+        { src: 'logo9.png' ,id: 'empty', correctPosition: 9 }
+    ];
+
+    parent = $('#game');
+
+    // Mélange les tuiles
+    tiles.sort(function() { 
+        return 0.5 - Math.random(); 
+    });
+
+    // Génère les tuiles
+    tiles.forEach(function(tile) {
+        $('<div>').attr('id', tile.id).addClass('tile').append($('<img>').attr('src', tile.src)).appendTo('#game');
+    });
+
+    // Ajoute un événement de clic à chaque tuile
+    $(document).on('click', '.tile', function() {
+        var emptyTile = $('#empty');
+        var clickedTile = $(this);
+    
+        // Calcule la différence de position entre la tuile cliquée et la tuile vide
+        var diff = Math.abs(clickedTile.index() - emptyTile.index());
+    
+        // Calcule les positions de ligne et de colonne de la tuile cliquée et de la tuile vide
+        var clickedTileRow = Math.floor(clickedTile.index() / 3);
+        var emptyTileRow = Math.floor(emptyTile.index() / 3);
+        var clickedTileCol = clickedTile.index() % 3;
+        var emptyTileCol = emptyTile.index() % 3;
+    
+        // Si les tuiles sont adjacentes horizontalement (et sur la même ligne) ou verticalement (et sur la même colonne), échange leurs positions
+        if ((diff === 1 && clickedTileRow === emptyTileRow) || (diff === 3 && clickedTileCol === emptyTileCol)) {
+            if (clickedTile.index() < emptyTile.index()) {
+                clickedTile.insertAfter(emptyTile);
+            } else {
+                clickedTile.insertBefore(emptyTile);
+            }
+        }
+    
+        // Vérifie si les tuiles sont dans le bon ordre
+        var isWin = tiles.every(function(tile, index) {
+            return $('#' + tile.id).index() === tile.correctPosition - 1;
+        });
+    
+        if (isWin) {
+            $('<p>').text('Vous avez gagné').css('color', 'green').appendTo('#game');
+            $(document).off('click', '.tile'); // Désactive les événements de clic
+        }
+    });
+
+    // Ajoute un bouton "Recommencer"
+    $('<button>').text('Recommencer').click(function() {
+        location.reload();
+    }).appendTo('#game');
+});
